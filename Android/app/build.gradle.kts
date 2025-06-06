8aad21e43873b6afb549a69fe61c79f9491a0cea
Mikael Laine <mikael.h.laine@gmail.com>
2025-06-06 20:46:31 +0300

    UPD: Screenshots and app icon

--- Android/app/build.gradle.kts
+++ Android/app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "com.memorylaine.tikki"
         minSdk = 24
         targetSdk = 34
-        versionCode = 2
-        versionName = "1.1"
+        versionCode = 3
+        versionName = "1.11"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
